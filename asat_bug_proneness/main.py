1262e2fb0ef696ab1610d4be9a27dd469f35116d
Anna Jancso <anna.t.jancso@gmail.com>
2020-08-20 15:23:05 +0200

    Script that clones projects & runs BugClassification

--- asat_bug_proneness/main.py
+++ asat_bug_proneness/main.py
@@ -1,15 +1,55 @@
 from tqdm import tqdm
+import docker
+from pathlib import Path
 
 from asat.util.downloader import RepoDownloader
 from asat.model.db import DB
 
-DB_PATH = '../asat/asat/data/projects.sqlite3'
-REPOS_PATH = '../asat/repos'
+# download repositories from ASAT database
 
-db = DB(DB_PATH)
-projects = db.get_projects()
+DB_PATH = Path('../asat/asat/data/projects.sqlite3').resolve()
+REPOS_PATH = Path('../asat/repos').resolve()
 
-downloader = RepoDownloader(REPOS_PATH)
+db = DB(str(DB_PATH))
+projects = db.get_projects()[:2]
+
+downloader = RepoDownloader(str(REPOS_PATH))
 
 for project in tqdm(projects):
     repo_path = downloader.download(project.url)
+
+# get the bugs from BugClassification
+
+IMAGE = 'bugclassification'
+CONTAINER = 'bugclassification'
+
+client = docker.from_env()
+
+print('Build image...')
+client.images.build(tag=IMAGE,
+                    path='../BugClassification')
+
+print('Remove running containers...')
+beewatch_containers = client.containers.list(all=True, filters={'ancestor': IMAGE})
+for container in beewatch_containers:
+    container.remove(force=True)
+
+print('Run new container...')
+container = client.containers.run(
+    detach=True,
+    image=IMAGE,
+    volumes={REPOS_PATH: {'bind': '/cloud_projects/', 'mode': 'rw'}}
+)
+
+status = container.status
+while status != 'exited':
+    container.reload()
+    status = container.status
+
+
+print('Get results for container {}...'.format(str(container)))
+bits, stat = container.get_archive('./BugFrequencies/output')
+
+with open('output.tar', 'wb') as f:
+    for chunk in bits:
+        f.write(chunk)
